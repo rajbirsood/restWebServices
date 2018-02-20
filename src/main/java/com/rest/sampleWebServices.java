05be104b4f180ee277e4fd923722c74e426b6f26
rajbirsood <rajbirs4ver@gmail.com>
2018-02-20 15:57:16 +0530

    initial commit

---
+++ src/main/java/com/rest/sampleWebServices.java
@@ -0,0 +1,33 @@
+package com.rest;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+@Path("/hello")
+public class sampleWebServices {
+    // This method is called if TEXT_PLAIN is request
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    public String sayPlainTextHello() {
+        return "Hello Webservice";
+    }
+
+    // This method is called if XML is request
+    @GET
+    @Produces(MediaType.TEXT_XML)
+    public String sayXMLHello() {
+        return "<?xml version=\"1.0\"?>" + "<hello> Hello Webservice" + "</hello>";
+    }
+
+    // This method is called if HTML is request
+    @GET
+    @Produces(MediaType.TEXT_HTML)
+    public String sayHtmlHello() {
+        return "<html> " + "<title>" + "Hello Webservice" + "</title>"
+                + "<body><h1>" + "Hello Webservice" + "</body></h1>" + "</html> ";
+    }
+
+
+}
